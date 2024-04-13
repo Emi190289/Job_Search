26777cd52ff27b75572457b481ff92730a83183c
Emi190289 <emi.priyadharsini@gmail.com>
2024-04-14 00:21:53 +0400

    modified

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
 'use client'
 import React from 'react'
-import Mainpage from '@/components/Mainpage';
 import Link from 'next/link';
 // import homepage from '../components/Homepage';
-import Navbarr from '@/components/Navbarr';
-
-import JobList from '@/components/JobList';
 import Homepage from '@/components/Homepage';
 
 
