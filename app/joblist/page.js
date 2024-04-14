79544eff270b5e8aac3d8604da6c75e6690e6b76
Emi190289 <emi.priyadharsini@gmail.com>
2024-04-14 13:27:23 +0400

    joblist.js modified

--- app/joblist/page.js
+++ app/joblist/page.js
@@ -1,66 +1,112 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, FormEvent } from 'react'
 import { CiSearch } from "react-icons/ci";
 import Footer from '@/components/Footer';
 import Navbarr from '@/components/Navbarr';
 import Image from 'next/image';
 
-const page = () => {
-    const [jobs, setJobs] = useState([]);
-    const [search,setSearch] = useState('')
-    React.useEffect(() => {
-  async function fetchJobs() {
-    const res = await fetch('/api');
-    const data = await res.json();
-    setJobs(data.jobs); 
-  }  fetchJobs();
-}, [jobs]);    
-  
-const handleChange = (event) =>{
-    setSearch(event.target.value)
-    
-}      
-    return (   
-      <div>
-        <Navbarr />
-       
-            <div className='grid grid-cols-1 m-10  md:grid-cols-3 md:items-center md:gap-2'>
-              <label className='hidden md:inline md:justify-self-end md:text-sm'>Jobs or Keywords</label>
-        <div className='border-2 bg-white p-3 rounded-lg flex items-center  px-6 py-4 sm:mx-16 md:border-none md:mx-0 md:px-0 md:ml-4' >
+const Page = () => {
+  const [jobs, setJobs] = useState([]);
+  const [selectedJob, setSelectedJob] = useState();
+  const [formData, setFormData] = useState({
+    keyword: 'Marketing',
+    category: 'Marketing'
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+  async function onSubmit(event) {
+    event.preventDefault();
+    setupData();
+    // ...
+  }
+  React.useEffect(() => {
+    setupData();
+  }, []);
 
-          <CiSearch className='basis-1/4 md:hidden '/>
-          <input className='text-xs basis-3/4 font-light focus:outline-none  inline md:justify-self-center md:border-2 md:p-4 md:rounded-lg  md:px-4 md:py-4' placeholder='Search jobs,skills' type='search'  onChange={handleChange}></input></div>
-       
-        <button className='border-2 p-3 text-white bg-[#56BC80] m-6 rounded-lg text-sm sm:mx-24 md:mx-8 md:p-4 md:justify-self-start md:px-10'>Find Jobs</button>
-        </div>
-        <div className='bg-[#56BC80] sm:grid sm:grid-cols-2 grid grid-cols-1  p-4 md:grid md:grid-cols-3 gap-5'>
-    { jobs && jobs.filter((job)=>{
-            return search.toLowerCase() === '' 
-            ? job
-            : job.jobTitle.toLowerCase().includes(search)
+  const setupData = async () => {
+    const response = await fetch('/api/jobs?' + new URLSearchParams({
+      keyword: formData.keyword,
+      category: formData.category.toLowerCase(),
+      count: 10
+    }), {
+      method: 'GET',
     })
-    
-              .map(job => (             
-                <ul key={job.id} className='rounded-xl shadow-xl bg-white text-black py-5 px-10  '>
-                <li className='mt-2'>
-                  <img src={job.companyLogo} className='max-w-[40px]' />
+
+    // Handle response if necessary
+    const data = await response.json();
+    setJobs(data.jobs);
+  };
+  return (
+    <div>
+      <Navbarr />
+      <div className='bg-[#56BC80] py-[60px] '>
+        <div className='max-w-screen-xl mx-auto'>
+          <form onSubmit={onSubmit}>
+            <div className='text-white text-4xl me-10'>
+              Search Jobs
+            </div>
+            <div className='max-w-screen-md flex items-center'>
+
+              <div className=' bg-white p-3 rounded-lg flex items-center px-10 py-4 h-[100px] ' >
+                <div className='flex flex-col'>
+                  <label className='text-xs text-semibold'>Keywords</label>
+                  <input onChange={handleChange} className='text-xs basis-3/4 font-light focus:outline-none  inline md:mr-2' placeholder='Search jobs,skills' name='keyword' type='text' />
+                </div>
+                <div className='flex flex-col'>
+                  <label>Category</label>
+                  <select className='text-sm' name='category' onChange={handleChange} value={formData.category}>
+                    <option value={'marketing'}>Marketing</option>
+                    <option value={'data-science'}>Data Science</option>
+                  </select>
+                </div>
+                <div className='flex flex-col'>
+                  <label>Location</label>
+                  <select className='text-sm' name='category' onChange={handleChange} value={formData.category}>
+                    <option value={'marketing'}>Marketing</option>
+                    <option value={'data-science'}>Data Science</option>
+                  </select>
+                </div>
+              </div>
+              <button className='border-2 px-4 py-2 text-white bg-[#56BC80] ml-4 rounded-lg text-sm'>Find Jobs</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className='bg-[#f1f1f1] py-[60px] pt-10'>
+        <div className='max-w-screen-xl mx-auto'>
+          <p className='text-2xl font-semibold mb-10'>Jobs List</p>
+          <div className='md:grid md:grid-cols-1 gap-5 '>
+            {jobs && jobs.length > 0 && jobs.map(job => (
+
+              <ul key={job.id} className='rounded-xl shadow-xl bg-white text-black py-5 px-10  '>
+
+                <li className='text-lg font-semibold mt-2'>
+                  {job.jobTitle}
+                </li>
+                <li className='text-sm font-semibold mt-2'>
+                  {job.companyName}
                 </li>
                 <li className=' my-2'>
                   <span className='bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded p-4'>{job.jobType}</span>
-                  </li>
-                <li className='text-sm font-semibold mt-2'>
-                  {job.jobTitle}
                 </li>
-                <li><small dangerouslySetInnerHTML={{__html: job.jobExcerpt }}></small></li>
+                <li><small dangerouslySetInnerHTML={{ __html: job.jobExcerpt }}></small></li>
                 <li className='text-xs mt-2'>Job Location: <em>{job.jobGeo}</em></li>
-                <li><button className=' text-xs px-2 py-1 border-2 rounded-xl text-[#56BC80] mt-2 border-green-400'>Apply Here</button></li>
+                <li className='text-right'><button className=' text-xs py-2 px-4 rounded-lg text-white bg-[#56BC80] mt-2'>Apply Here</button></li>
               </ul>
-            
-             ))}
+            ))}
           </div>
-        
-      <Footer />
+        </div>
       </div>
-    )
-    }
-export default page
+
+
+
+      <Footer />
+    </div >
+  )
+}
+export default Page
--- components/Feature.js
+++ components/Feature.js
@@ -15,7 +15,7 @@ const Feature = () => {
         <div className='my-4 animate__animated animate__fadeInDown animate__delay-3s'>   <Image src='/drop.png' width={150} height={50} alt="drop" /></div>
       </div>
       <div className='p-4'>  <p className='text-center text-lg font-semibold mt-10'>How JOBS can help</p>
-        <p className='text-center text-xs max-w-screen-sm mx-auto my-6 '>We are the largest independent job board, register your CV to apply today with simple search tool and instant job matches delivered to your inbox, it's never been easier to land a new job with us </p></div>
+        <p className='text-center text-xs max-w-screen-sm mx-auto my-6 '>We are the largest independent job board, register your CV to apply today with simple search tool and instant job matches delivered to your inbox, it&apos;s never been easier to land a new job with us </p></div>
       <div className='px-4 '><Itemss /></div>
     </div>
 
